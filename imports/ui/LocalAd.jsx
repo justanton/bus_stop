beaf71031ef76ad2284308ccba46c09a04852b4c
Tomi Kalmi <tomi.kalmi@aalto.fi>
2016-11-27 01:19:25 +0200

    Implement crude version of the main menu

--- imports/ui/LocalAd.jsx
+++ imports/ui/LocalAd.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 class LocalAd extends React.Component {
     render() {
-      let className = this.props.active ? 'active' : ''
+      let className;
+      switch (this.props.mode) {
+        case 'main-fullscreen':
+          className = 'hidden'
+          break;
+        default:
+          className = 'active'
+          break;
+      }
         return (
             <section id="local-ad" className={className}>
                 <h1>
